2269299287b4fca85fca3544897977ded81f0181
tokj <tokj@itu.dk>
2022-03-03 14:10:49 +0100

    added something that kinda works

--- OthelloProject/ToreBOT.java
+++ OthelloProject/ToreBOT.java
@@ -1,9 +1,9 @@
+import java.io.Console;
 import java.io.PipedOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ToreBOT implements IOthelloAI {
-
-
     
     public Position decideMove(GameState s) {
         return oneDepth(s);
@@ -17,13 +17,16 @@ public class ToreBOT implements IOthelloAI {
         var bestTokens = Integer.MIN_VALUE;
         //var gameStates = new ArrayList<GameState>();
         for (Position position : LegalMoves) {
-            var CurrentState = s;
-            CurrentState.insertToken(position);
-            var nmbrTokens = CurrentState.countTokens()[0];
-            if(nmbrTokens>bestTokens){
-                bestMove = position;
-            }
-            
+            var copyState = s;
+            if (copyState.insertToken(position)) {
+                var nmbrTokens = copyState.countTokens()[0];
+                System.out.println("Row =  " + position.row + " Col = " + position.col + "\n");
+                System.out.println(Arrays.deepToString(copyState.getBoard()).replace("], ", "]\n").replace("[[", "[").replace("]]", "]") + "\n");
+                if(nmbrTokens>bestTokens){
+                    bestMove = position;
+                }
+            }  
+            else return new Position (-1,-1);        
         }
         return bestMove;
     }
